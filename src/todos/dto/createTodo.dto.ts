cb40f08520cf2daf8f016582d0a1388a946ba68b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-13 16:39:47 +0300

    поля dto

--- src/todos/dto/createTodo.dto.ts
+++ src/todos/dto/createTodo.dto.ts
@@ -1,6 +1,10 @@
-import { IsNotEmpty } from "class-validator";
+import { IsBoolean, IsNotEmpty } from "class-validator";
 
 export class CreateTodo {
     @IsNotEmpty()
   readonly title: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  readonly done: boolean;
 }
