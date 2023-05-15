02d576ce23acf846fdd4bea8748f3b00540b85d9
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-15 11:52:45 +0300

    create контроллер

--- src/todos/todo.controller.ts
+++ src/todos/todo.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateTodo } from './dto/createTodo.dto';
 import { TodoService } from './todo.service';
 
-@Controller("todos")
+@Controller('todos')
 export class TodoContainer {
   constructor(private readonly todoService: TodoService) {}
 
@@ -10,8 +20,15 @@ export class TodoContainer {
     return this.todoService.findAll();
   }
 
-  @Get(":id")
-  GetTodoById(@Param("id") id: string) {
+  @Get(':id')
+  GetTodoById(@Param('id') id: string) {
     return this.todoService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-Type', 'application/json')
+  CreateTodo(@Body() createTodo: CreateTodo) {
+    return this.todoService.create(createTodo);
   }
 }
