7b8ad98187cbcc96ce54cd9350f1dc677dd87e9a
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-16 23:20:45 +0300

    фикс

--- src/todos/todo.module.ts
+++ src/todos/todo.module.ts
@@ -1,12 +1,12 @@
-import { Module } from "@nestjs/common";
-import { SequelizeModule } from "@nestjs/sequelize";
-import { TodoModel } from "./models/todo.model";
-import { TodoController } from "./todo.controller";
-import { TodoService } from "./todo.service";
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { TodoModel } from './models/todo.model';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 @Module({
-    imports: [SequelizeModule.forFeature([TodoModel])],
-    providers:[TodoService],
-    controllers:[TodoController],
+  imports: [SequelizeModule.forFeature([TodoModel])],
+  providers: [TodoService],
+  controllers: [TodoController],
 })
 export class TodoModule {}
